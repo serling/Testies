31dbcbe2bdca6aa34f236111f5e510d92f981369
thomasc <frostu@gmail.com>
2020-11-12 09:03:15 +0100

    mouseevent script
    mouse event script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,29 +9,13 @@ using Testies.Models;
 public class GameManager : MonoBehaviour
 {
 
-    public InputEvent onMouseClick;
-
-    private void PrintName(GameObject go)
-    {
-        print(go.name);
-    }
-
     private void Start()
     {
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) //TODO: extract this into event 
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, 100.0f))
-            {
-                onMouseClick.Raise(hit.transform.gameObject);
-            }
-        }
+        
         
     }
 
---
+++ Assets/Scripts/MouseClickHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Testies.Events;
+
+public class MouseClickHandler : MonoBehaviour
+{
+    public InputEvent onMouseClick;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                onMouseClick.Raise(hit.transform.gameObject);
+            }
+        }
+    }
+}
